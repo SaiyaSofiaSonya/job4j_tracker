62da385a0c3c55a5351efa5a97eb1ded14352cad
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-10-09 08:59:05 +0200

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,24 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int sizeWithoutNull = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                rsl[index] = item;
+                sizeWithoutNull++;
+            }
+        }
+        return Arrays.copyOf(rsl, sizeWithoutNull);
+    }
+
+    public Item[] findAll() {
+        Item[] namesWithoutNull;
+        namesWithoutNull = Arrays.copyOf(items, size);
+        return namesWithoutNull;
     }
 }
