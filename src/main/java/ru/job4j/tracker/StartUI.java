87cdb0d36f57f480bb93438805504ea1992962c0
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-10-17 09:02:25 +0200

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745 #216711]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -26,8 +26,8 @@ public class StartUI {
                     }
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
-                    }
-                } else if (select == 6) {
+                }
+            } else if (select == 6) {
                 run = false;
             }
         }
