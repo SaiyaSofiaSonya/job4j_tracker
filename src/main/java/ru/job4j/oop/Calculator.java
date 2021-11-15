5c32070198d1c45d680f151667f5b24de9f440ec
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-11-15 07:29:52 +0200

    5. Input и полиморфизм. [#181094 #222041]

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return y - x;
+    }
+
+    public int divide(int y) {
+        return y / x;
+    }
+
+    public  int sumAllOperation(int y) {
+        return sum(y) + minus(y) + divide(y);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = Calculator.sum(10);
+        System.out.println(result);
+        result = Calculator.minus(10);
+        System.out.println(result);
+        result = calculator.divide(10);
+        System.out.println(result);
+        result = calculator.sumAllOperation(10);
+        System.out.println(result);
+    }
+}
