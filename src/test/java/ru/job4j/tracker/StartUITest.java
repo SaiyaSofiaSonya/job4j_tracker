f8faceb88052c1ef1ffe270ab476e5dfa899d1c0
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-11-25 08:17:36 +0200

    9. Написать тесты на StartUI. [#785 #231438]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -45,7 +47,7 @@ public class StartUITest{
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(new String[]
-                {"0","1", "1"});
+                {"0",String.valueOf(one.getId()), "1"});
         UserAction[] actions = {
                 new DeleteAction(out),
                 new ExitProgramAction(out)
@@ -95,6 +97,36 @@ public class StartUITest{
                         + "Заявка изменена успешно." + ln
                         + "Menu." + ln
                         + "0. Edit item" + ln
+                        + "1. Exit Program" + ln
+                )
+        );
+    }
+
+    @Test
+    public void whenFindAllOutput() {
+        final DateTimeFormatter FORMATTER = DateTimeFormatter
+                .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input in = new StubInput(new String[] {
+                "0",
+                "1"
+        });
+        UserAction[] actions = {
+                new FindAllAction(out),
+                new ExitProgramAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                  "Menu." + ln
+                        + "0. Show all items" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Show all items ====" + ln
+                        + "Item{id=1, name='test1', created=" + one.getCreated().format(FORMATTER) + "}" + ln
+                        + "Menu." + ln
+                        + "0. Show all items" + ln
                         + "1. Exit Program" + ln
                 )
         );
