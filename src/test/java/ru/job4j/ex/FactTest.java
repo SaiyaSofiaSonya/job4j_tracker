cc730f1c7ff84df948f2cca8e26e1249d27fd867
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-11-28 19:13:52 +0200

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalcException() {
+        int in = -1;
+        Fact fact = new Fact();
+        fact.calc(in);
+
+    }
+}
