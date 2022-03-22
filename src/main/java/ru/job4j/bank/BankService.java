3af51a70448c0a34e10e66177a0e72f2bdf78d4a
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2022-03-23 00:02:06 +0200

    3. Банковские переводы [#10038 #278713]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,19 +9,18 @@ public class BankService {
 private final Map<User, List<Account>> users = new HashMap<>();
 
    public void addUser(User user) {
-    if (!users.containsKey(user.getPassport())) {
-        users.put(user, new ArrayList<>());
-    }
+    users.putIfAbsent(user, new ArrayList<>());
    }
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
+        if (user != null) {
         List<Account> accounts = users.get(user);
-        if (user != null && !accounts.contains(account)) {
+        if (!accounts.contains(account)) {
             accounts.add(account);
-            users.put(user, accounts);
         }
     }
+   }
 
     public User findByPassport(String passport) {
         User user = null;
@@ -42,6 +41,7 @@ private final Map<User, List<Account>> users = new HashMap<>();
             for (Account each : accounts) {
                 if (each.getRequisite().equals(requisite)) {
                     account = each;
+                    break;
                 }
             }
         }
@@ -51,23 +51,13 @@ private final Map<User, List<Account>> users = new HashMap<>();
     public boolean transferMoney(String srcPassport, String srcRequisite,
                           String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        User srcUser = findByPassport(srcPassport);
-        User destUser = findByPassport(destPassport);
-        List<Account> srcAccounts = users.get(srcUser);
-        List<Account> destAccounts = users.get(destUser);
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
         Account destAccount = findByRequisite(destPassport, destRequisite);
         if (srcAccount != null
+                && destAccount != null
                 && srcAccount.getBalance() >= amount) {
-            srcAccounts.set(
-                    srcAccounts.indexOf(srcAccount),
-                    new Account(srcRequisite, srcAccount.getBalance() - amount));
-            users.put(srcUser, srcAccounts);
-            destAccounts.set(
-                    destAccounts.indexOf(destAccount),
-                    new Account(destRequisite, destAccount.getBalance() + amount)
-            );
-            users.put(destUser, destAccounts);
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
             rsl = true;
         }
         return rsl;
