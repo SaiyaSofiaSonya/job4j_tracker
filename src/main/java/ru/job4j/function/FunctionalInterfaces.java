68801f933815c7b490184868c8bb4b9d33b7e82c
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2022-03-31 09:51:40 +0200

    1.2. Функциональный интерфейс [#247497 #282551]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -18,7 +18,7 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (s, s1) -> (s % 2 == 0 || map.get(s).length() == 4);
+        BiPredicate<Integer, String> biPred = (s, s1) -> (s % 2 == 0 || s1.length() == 4);
         for (Integer i : map.keySet()) {
             System.out.println("key: " + biPred.test(i, map.get(i)) + " value: " + map.get(i));
         }
